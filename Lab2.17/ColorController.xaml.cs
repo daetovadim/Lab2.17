d78024526c20a46c94bb1eca6ec3f5f09320f399
Вадим Малутин <malutin.vadim@yandex.ru>
2022-01-30 21:10:58 +0600

    Add project files.

---
+++ Lab2.17/ColorController.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2._17
+{
+    /// <summary>
+    /// Interaction logic for ColorController.xaml
+    /// </summary>
+    public partial class ColorController : UserControl
+    {
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(
+                nameof(Color),
+                typeof(Color),
+                typeof(ColorController),
+                new FrameworkPropertyMetadata(Colors.Black, new PropertyChangedCallback(OnColorChanged)));
+
+        public static readonly DependencyProperty RedProperty =
+            DependencyProperty.Register(
+                nameof(Red),
+                typeof(byte),
+                typeof(ColorController),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+        public static readonly DependencyProperty GreenProperty =
+            DependencyProperty.Register(
+                nameof(Green),
+                typeof(byte),
+                typeof(ColorController),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+
+        public static readonly DependencyProperty BlueProperty =
+            DependencyProperty.Register(
+                nameof(Blue),
+                typeof(byte),
+                typeof(ColorController),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback(OnColorRGBChanged)));
+        
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+        public byte Red
+        {
+            get => (byte)GetValue(RedProperty);
+            set => SetValue(RedProperty, value);
+        }
+        public byte Green
+        {
+            get => (byte)GetValue(GreenProperty);
+            set => SetValue(GreenProperty, value);
+        }
+        public byte Blue
+        {
+            get => (byte)GetValue(BlueProperty);
+            set => SetValue(BlueProperty, value);
+        }
+
+        private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+            ColorController colorController = (ColorController)sender;
+            colorController.Red = newColor.R;
+            colorController.Green = newColor.G;
+            colorController.Blue = newColor.B;
+        }
+        private static void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorController colorController = (ColorController)sender;
+            Color color = colorController.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorController.Color = color;
+        }
+
+        public ColorController()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Top = Properties.Settings.Default.Position.Top;
+            this.Left = Properties.Settings.Default.Position.Left;
+            this.Height = Properties.Settings.Default.Position.Height;
+            this.Width = Properties.Settings.Default.Position.Width;
+
+            InitializeComponent();
+            mcolor = new ColorRGB();
+            mcolor.red = 0;
+            mcolor.green = 0;
+            mcolor.blue = 0;
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Position = this.RestoreBounds;
+            Properties.Settings.Default.Save();
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Только картинки";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Jpg.Name = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Только текстовые файлы (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, Jpg.Name);
+            }
+        }
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Jpg.EditingMode = InkCanvasEditingMode.Ink;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Jpg.EditingMode = InkCanvasEditingMode.Select;
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Jpg.EditingMode = InkCanvasEditingMode.InkAndGesture;
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Jpg.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            this.Jpg.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+        public class ColorRGB
+        {
+            public byte red { get; set; }
+            public byte green { get; set; }
+            public byte blue { get; set; }
+        }
+        public ColorRGB mcolor { get; set; }
+        public Color clr { get; set; }
+        private void sld_Color_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            string crlName = slider.Name; //Определяем имя контрола, который покрутили
+            double value = slider.Value; // Получаем значение контрола
+                                         //В зависимости от выбранного контрола, меняем ту или иную компоненту и переводим ее в тип byte
+            if (crlName.Equals("sld_RedColor"))
+            {
+                mcolor.red = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_GreenColor"))
+            {
+                mcolor.green = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_BlueColor"))
+            {
+                mcolor.blue = Convert.ToByte(value);
+            }
+            //Задаем значение переменной класса clr 
+            clr = Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue);
+            //Устанавливаем фон для контрола Label 
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
+            // Задаем цвет кисти для контрола InkCanvas
+            this.Jpg.DefaultDrawingAttributes.Color = clr;
+        }
+
+    }
+}
